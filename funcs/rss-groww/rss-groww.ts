c9f394a8e83ed955323f2870834a2f40ef1cc5bd
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2022-08-11 21:26:36 +0530

    basic fetch to groww

--- funcs/rss-groww/rss-groww.ts
+++ funcs/rss-groww/rss-groww.ts
@@ -1,12 +1,45 @@
+import 'dotenv/config'
 import { Handler } from '@netlify/functions'
+import fetch from 'node-fetch'
+import type { Response } from 'node-fetch'
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
+  let res: Response
+  try {
+    res = await fetch(`${process.env.GROWW_HOST}/wp-json/wp/v2/daily-digest?page=1&per_page=5`)
+  } catch {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `Upstream error`,
+      }),
+    }
+  }
+
+  if (!res.headers.get('content-type')?.includes('json')) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `Server error`,
+      }),
+    }
+  }
+
+  if (!res.ok) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: `Bad request`,
+      }),
+    }
+  }
+
+  const data = await res.json()
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${name}!`,
+      message: data,
     }),
   }
 }
