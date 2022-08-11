17c22b76c94f8864e63cf409e4d7b1684931aad8
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2022-08-11 22:32:12 +0530

    map groww response to rss feed

---
+++ funcs/rss-groww/feed.ts
@@ -0,0 +1,33 @@
+import RSS from 'rss'
+
+export type DailyDigest = {
+    title: {
+        rendered: string
+    },
+    date_gmt: string,
+    modified_gmt: string,
+    slug: string,
+    digest_content: {
+        intro: string,
+    }
+}
+
+export const feed = (digests: DailyDigest[]): string => {
+    const rss = new RSS({
+        title: 'Groww Digest - Daily',
+        feed_url: `${process.env.GROWW_FEED_URL}`,
+        site_url: `${process.env.GROWW_SITE_URL}`,
+    })
+
+    digests.forEach(d => {
+        rss.item({
+            title: d.title.rendered,
+            description: d.digest_content.intro,
+            url: `${process.env.GROWW_DAILY_DIGEST_HOST}/${d.slug}`,
+            date: d.date_gmt,
+            guid: d.slug,
+        })
+    })
+
+    return rss.xml()
+}
--- funcs/rss-groww/rss-groww.ts
+++ funcs/rss-groww/rss-groww.ts
@@ -2,43 +2,42 @@ import 'dotenv/config'
 import { Handler, HandlerResponse } from '@netlify/functions'
 import fetch from 'node-fetch'
 import type { Response } from 'node-fetch'
+import { DailyDigest, feed } from './feed'
 
 export const handler: Handler = async (event, context) => {
   let res: Response
   try {
     res = await fetch(`${process.env.GROWW_HOST}/wp-json/wp/v2/daily-digest?page=1&per_page=5`)
   } catch {
-    return buildResponse(500, {
-      message: `Upstream error`,
-    })
+    return buildResponse(500, `Upstream error`)
   }
 
   if (!res.ok) {
-    return buildResponse(400, {
-      message: `Bad request`,
-    })
+    return buildResponse(400, `Bad request`)
   }
 
   if (!res.headers.get('content-type')?.includes('json')) {
-    return buildResponse(404, {
-      message: `Daily digest not found`,
-    })
+    return buildResponse(404, `Daily digest not found`)
   }
 
   const data = await res.json()
 
-  return buildResponse(200, data)
+  let xml
+  try {
+    xml = feed(data as DailyDigest[])
+  } catch {
+    return buildResponse(500, `Server error`)
+  }
+
+  return buildResponse(200, xml)
 }
 
-function buildResponse(statusCode: number, data: any): HandlerResponse {
+function buildResponse(statusCode: number, body: string): HandlerResponse {
   return {
     statusCode,
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/atom+xml'
     },
-    body: JSON.stringify({
-      success: statusCode >= 200 && statusCode < 400,
-      data,
-    })
+    body,
   }
 }
