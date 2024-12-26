3e1b04e2184356e9e2d344ef0a327db575f16f2b
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2024-12-26 16:55:22 +0530

    add logging

--- funcs/rss-bytes/rss-bytes.ts
+++ funcs/rss-bytes/rss-bytes.ts
@@ -6,8 +6,17 @@ import { fetchPosts } from './upstream'
 import { feed } from './feed'
 import { headerKeyETag, headerKeyIfNoneMatch, successResponse } from '../../common/http'
 import { blanked } from '../../common/util'
+import { pino } from 'pino'
+
+const logger = pino({
+    level: process.env.LOG_LEVEL,
+    base: { func: "rss-bytes" },
+})
 
 export const handler: Handler = async (event, _context) => {
+    const config = getConfig(process.env)
+    logger.debug(config, "using config")
+
     const response = await fetchPosts(blanked(process.env.BYTES_HOST), blanked(event.headers[headerKeyIfNoneMatch]));
 
     // https://www.typescriptlang.org/docs/handbook/2/narrowing.html#discriminated-unions
@@ -22,4 +31,23 @@ export const handler: Handler = async (event, _context) => {
         .otherwise(_res => {
             return { statusCode: 500 }
         })
+}
+
+function getConfig(env: any) {
+    const {
+        BYTES_FEED_TITLE,
+        BYTES_FEED_URL,
+        BYTES_SITE_URL,
+        BYTES_IMG_URL,
+        BYTES_FEED_ITEM_BASE_URL,
+        BYTES_HOST,
+    } = env
+    return {
+        BYTES_FEED_TITLE,
+        BYTES_FEED_URL,
+        BYTES_SITE_URL,
+        BYTES_IMG_URL,
+        BYTES_FEED_ITEM_BASE_URL,
+        BYTES_HOST,
+    }
 }
--- funcs/rss-groww/rss-groww.ts
+++ funcs/rss-groww/rss-groww.ts
@@ -9,8 +9,17 @@ import {
 } from '../../common/http';
 import { blanked } from '../../common/util';
 import { match } from 'ts-pattern';
+import { pino } from 'pino'
+
+const logger = pino({
+    level: process.env.LOG_LEVEL,
+    base: { func: "rss-groww" },
+})
 
 export const handler: Handler = async (event, _context) => {
+    const config = getConfig(process.env)
+    logger.debug(config, "using config")
+
     const response = await fetchDigests(blanked(process.env.GROWW_HOST), blanked(event.headers[headerKeyModifiedSince]))
 
     return match(response)
@@ -24,4 +33,23 @@ export const handler: Handler = async (event, _context) => {
         .with({ kind: 'error', statusCode: 404 }, _res => failureResponse(404, `Daily digest not found`))
         .with({ kind: 'error' }, _res => failureResponse(400, `rsstree bad request`))
         .otherwise(_res => failureResponse(500, `rsstree server error`))
+}
+
+function getConfig(env: any) {
+    const {
+        GROWW_HOST,
+        GROWW_FEED_TITLE,
+        GROWW_FEED_ITEM_BASE_URL,
+        GROWW_FEED_URL,
+        GROWW_SITE_URL,
+        GROWW_IMG_URL,
+    } = env
+    return {
+        GROWW_HOST,
+        GROWW_FEED_TITLE,
+        GROWW_FEED_ITEM_BASE_URL,
+        GROWW_FEED_URL,
+        GROWW_SITE_URL,
+        GROWW_IMG_URL,
+    }
 }
